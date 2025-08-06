27aa2c430736fb431846599707600c8c2b725c94
anonn78103 <139428061+anonn78103@users.noreply.github.com>
2025-08-06 12:26:35 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -34,16 +34,6 @@ app.use("/api/contacts", contactsRoutes);
 app.use("/api/messages", messagesRoutes);
 app.use("/api/channel", channelRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
-
-  app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
-  });
-}
-
-
-
 
 const server = app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
